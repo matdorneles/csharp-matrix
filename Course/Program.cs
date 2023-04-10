6f0fc8d014f63bad1cd888eaddcbb1cd2faf70a5
Matheus Dorneles <matheus.sdorneles1@gmail.com>
2023-04-10 11:03:45 -0300

    Add project files.

---
+++ Course/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+
+namespace Aulas {
+    class Aulas {
+        static void Main(string[] args) {
+
+            string[] size = Console.ReadLine().Split(" "); 
+            int m = int.Parse(size[0]);
+            int n = int.Parse(size[1]);
+
+            int[,] mat = new int[m, n];
+            for (int i = 0; i < m; i++) {
+                string[] line = Console.ReadLine().Split(" ");
+                for (int j = 0; j < n; j++) {
+                    mat[i, j] = int.Parse(line[j]);
+                }
+            }
+
+            int x = int.Parse(Console.ReadLine());
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (mat[i, j] == x) {
+                        Console.WriteLine($"Position {i}, {j}:");
+                        if (j > 0)
+                            Console.WriteLine($"Left: {mat[i, j - 1]}");
+                        if (i > 0)
+                            Console.WriteLine($"Up: {mat[i - 1, j]}");
+                        if (j < n - 1)
+                            Console.WriteLine($"Right: {mat[i, j + 1]}");
+                        if (i < m - 1)
+                            Console.WriteLine($"Down: {mat[i + 1, j]}");
+                    }
+                }
+            }
+        }
+    }
+}
